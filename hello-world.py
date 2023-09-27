16bf5b91599de7caa970e5aa8f25f1f53b00237f
incarbonell <145944513+incarbonell@users.noreply.github.com>
2023-09-27 08:48:21 +0800

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+
+name = input("what is your name?")
+age= int (input("what is your age?"))
+pi = 3.14
+
+
+print(f"Hello, {name}")
+print(f"My age next year is{(age+1)}")
+print (f"pi value is {pi}")

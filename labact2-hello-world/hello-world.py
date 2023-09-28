9bcb5d873c4b4c45d6c71d788610fc5f0f0fe03b
incarbonell <145944513+incarbonell@users.noreply.github.com>
2023-09-28 08:30:38 +0800

    hello-world.py

--- labact2-hello-world/hello-world.py
+++ labact2-hello-world/hello-world.py
@@ -1,9 +1,13 @@
 
-name = input("what is your name?")
+# Enter name
+name = input("What is your name?")
+# Enter age
 age= int (input("what is your age?"))
+
 pi = 3.14
 
-
 print(f"Hello, {name}")
+
 print(f"My age next year is{(age+1)}")
+
 print (f"pi value is {pi}")
